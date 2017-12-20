b324a2a818e13b6cd7e2a180d0d5b7360d830017
Nazanin <n.zinouri@gmail.com>
2017-12-19 21:14:32 -0500

    initial profanity check

---
+++ profanity_editor.py
@@ -0,0 +1,21 @@
+
+import urllib
+
+def read_text():
+    quotes = open(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\send-a-text\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print contents_of_file
+
+    check_profanity(contents_of_file)
+
+    quotes.close()
+
+def check_profanity(text_to_ckeck):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_ckeck)
+    output = connection.read()
+    print output
+
+    connection.close()
+
+
+read_text()
